4bb8520ae8af6f21ab8dce77500bdd5e525a93e8
sehyeon1104 <sehyeon1104@naver.com>
2022-11-27 16:50:58 +0900

    이동 구현

--- Assets/01.Script/Sehyeon/MonsterInput.cs
+++ Assets/01.Script/Sehyeon/MonsterInput.cs
@@ -4,23 +4,17 @@ using UnityEngine;
 using UnityEngine.Events;
 public class MonsterInput : MonoBehaviour
 {
-    Animator monsterAnimtor;
     [Header("몬스터가 입력받는 모든것을 넣는 스크립트")]
-    CharacterController monsterController;
+
     [SerializeField] UnityEvent<Vector3> moveKeyPress;
-    private void Awake()
-    {
-        monsterAnimtor= GetComponent<Animator>();
-        monsterController= GetComponent<CharacterController>();
-    }
+    [SerializeField] UnityEvent AttackButtonPress;
     void Update()
     {
         MonsterMove();  
     }
     void MonsterMove()
     {
-        moveKeyPress?.Invoke(new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")));
-        monsterAnimtor.SetBool("Walk", true);
+        moveKeyPress?.Invoke(new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized);
     }
     
 }
--- Assets/01.Script/Sehyeon/MonsterMovement.cs
+++ Assets/01.Script/Sehyeon/MonsterMovement.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class MonsterMovement : MonoBehaviour
 {
     CharacterController monstercontroller;
+    Animator monsterAni;
     [SerializeField]
-    [Range(0f,10f)]
-    float speed;
+    [Range(0f, 10f)]
+    float speed = 5;
+
+    readonly int horizontal = Animator.StringToHash("Horizontal");
+    readonly int vertical = Animator.StringToHash("Vertical");
     private void Awake()
     {
       monstercontroller= GetComponent<CharacterController>();
+        monsterAni= GetComponent<Animator>();
     }
     public void MoveMonster(Vector3 moveInput)
     {
         monstercontroller.SimpleMove(moveInput*speed);
+        monsterAni.SetFloat(horizontal,moveInput.x);
+        monsterAni.SetFloat(vertical,moveInput.z);
     }
 
   
