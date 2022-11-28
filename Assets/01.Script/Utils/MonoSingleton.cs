ad202b763c3af6f5cd729482733470f7d3b31a65
Minkyu Kwak <lanilani7661@gmail.com>
2022-11-28 23:24:26 +0900

    MonoSingleton 추가

---
+++ Assets/01.Script/Utils/MonoSingleton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = (T)FindObjectOfType(typeof(T));
+                if (instance == null)
+                {
+                    GameObject temp = new GameObject(typeof(T).ToString());
+                    instance = temp.AddComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+}
