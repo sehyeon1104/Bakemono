102f1fe073bbcca18dafaedcf3c328c6e866d58f
Minkyu Kwak <lanilani7661@gmail.com>
2022-11-30 12:23:27 +0900

    암호 입력 및 성공여부 체크

--- Assets/01.Script/Minkyu/PasswordManager.cs
+++ Assets/01.Script/Minkyu/PasswordManager.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
 
 public class PasswordManager : MonoBehaviour
 {
     private string password = "";
-    //int[] passwords = new int[4];
+    [SerializeField]
+    private TextMeshProUGUI[] inputPasswords = new TextMeshProUGUI[4];
+    int pivot = -1;
 
     private void Start()
     {
@@ -21,9 +26,51 @@ public class PasswordManager : MonoBehaviour
             num = Random.Range(0, 10);
             temp = num.ToString();
             password += temp;
-            //passwords[i] = num;
         }
 
         Debug.Log("Password : " + password);
     }
+
+    public void InputPassword()
+    {
+        string n = "";
+        GameObject clickObject = EventSystem.current.currentSelectedGameObject;
+        n = clickObject.GetComponentInChildren<TextMeshProUGUI>().text;
+
+        pivot++;
+
+        if(pivot > 3)
+        {
+            return;
+        }
+
+        inputPasswords[pivot].text = n;
+    }
+
+    public void BackPassword()
+    {
+        if(pivot < 0)
+        {
+            pivot = 0;
+        }
+
+        inputPasswords[pivot].text = "0";
+        pivot--;
+    }
+
+    public void EnterPassword()
+    {
+        for(int i = 0; i < 4; ++i)
+        {
+            if(inputPasswords[i].text.ToString() != password[i].ToString())
+            {
+                Debug.Log("Error!");
+                return;
+            }
+        }
+
+        Debug.Log("Succeed!");
+    }
+
+
 }
