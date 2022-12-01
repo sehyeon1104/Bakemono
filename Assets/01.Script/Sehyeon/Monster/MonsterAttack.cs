2027ca1254661fe4f7a819b88716b778f5e14c03
sehyeon1104 <sehyeon1104@naver.com>
2022-12-02 00:54:54 +0900

    공격 모션

--- Assets/01.Script/Sehyeon/Monster/MonsterAttack.cs
+++ Assets/01.Script/Sehyeon/Monster/MonsterAttack.cs
@@ -4,25 +4,53 @@ using UnityEngine;
 
 public class MonsterAttack : MonoBehaviour
 {
+    [SerializeField]
+    bool isRight = false;
+    bool isClick = true;
+    bool once = true;
+    float totalTime = 0;
+    float animTime = 0;
     Animator monsterAni;
     readonly int leftAttack = Animator.StringToHash("LeftAttack");
     readonly int rightAttack = Animator.StringToHash("RightAttack");
+    bool isLeft = true;
     void Start()
     {
-        monsterAni= GetComponent<Animator>();
+        monsterAni = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-   public void Attack(bool isLeft)
+    private void Update()
     {
-      if(isLeft)
-        {
-            monsterAni.SetTrigger(leftAttack);
-            
-        }
-      else if(!isLeft)
-        {
-            monsterAni.SetTrigger(rightAttack);
-        }
+        Attack();
     }
+    void Attack()
+    {
+        totalTime += Time.deltaTime;
+        if (Input.GetMouseButtonDown(0) && isClick == true)
+        {
+            isClick = false;
+            totalTime = 0;
+            if (isLeft) //왼쪽 공격
+            {
+                monsterAni.SetTrigger(leftAttack);
+                isLeft = false;
+            }
+            else//오른쪽
+            {
+                monsterAni.SetTrigger(rightAttack);
+                isLeft = true;
+            }
+        }
+        if (monsterAni.GetCurrentAnimatorStateInfo(1).normalizedTime >= 0.99f)
+        {
+            isClick = true;
+        }
+        if (totalTime > 15f)
+        {
+            isLeft = true;
+        }
+
+    }
+
+
 }
--- Assets/01.Script/Sehyeon/Monster/MonsterInput.cs
+++ Assets/01.Script/Sehyeon/Monster/MonsterInput.cs
@@ -5,18 +5,13 @@ using UnityEngine.Events;
 public class MonsterInput : MonoBehaviour
 {
     [Header("몬스터가 입력받는 모든것을 넣는 스크립트")]
-    bool isLeft =true;
-    [SerializeField] UnityEvent<Vector3> moveKeyPress;
-    [SerializeField] UnityEvent<bool> attackButtonPress;
+
+    [SerializeField] UnityEvent<Vector3> moveKeyPress;  
     void Update()
     {
        
         MonsterMove();
-        if(Input.GetMouseButtonDown(0))
-        {
-            isLeft = !isLeft;
-            attackButtonPress?.Invoke(isLeft);
-        }
+        
     }
    public void MonsterMove()
     {
