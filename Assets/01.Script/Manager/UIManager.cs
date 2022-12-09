617eeb0f967a528f5903860d2c6be96f29eb9134
Minkyu Kwak <lanilani7661@gmail.com>
2022-12-09 10:15:46 +0900

    QuestManager, UIManager 추가, User 위치 변경

---
+++ Assets/01.Script/Manager/UIManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    [Header("QuestUI")]
+    public TextMeshProUGUI questTitle;
+    public TextMeshProUGUI questContent;
+
+
+}
