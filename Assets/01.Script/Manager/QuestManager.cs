6b0691c9e7c96e168fa7b3940672d54f1638b649
Minkyu Kwak <lanilani7661@gmail.com>
2022-12-13 20:27:29 +0900

    QuestManager 수정 및 파일 경로 변경

--- Assets/01.Script/Manager/QuestManager.cs
+++ Assets/01.Script/Manager/QuestManager.cs
@@ -7,7 +7,8 @@ public class QuestManager : MonoSingleton<QuestManager>
     public bool doQuest { private set; get; } = false;
     public bool isClear { private set; get; } = false;
 
-    private int spawnResearchersCount = 0;
+    public int killedNpcCount = 0;
+    
 
     private void Start()
     {
@@ -15,36 +16,56 @@ public class QuestManager : MonoSingleton<QuestManager>
         isClear = false;
     }
 
-    void EnterKitchen()
+    private void EnterKitchen()
     {
         Debug.Log("Enter Kitchen");
-        StartCoroutine(KitchenQuest());
-    }
+        UIManager.Instance.questTitle.text = "식당에서 여유롭게 아직 음식을 먹고 있어?";
+        UIManager.Instance.questContent.text = "식당에있는 사람을 모두 죽이세요!";
 
-    IEnumerator KitchenQuest()
-    {
+        doQuest = true;
+
         while (!isClear)
         {
-            if(spawnResearchersCount == 0)
+            if (killedNpcCount >= 20)
             {
                 isClear = true;
                 doQuest = false;
             }
-
-            yield return null;
+            else
+            {
+                continue;
+            }
         }
-
-        yield break;
     }
 
     void EnterLaborator()
     {
         Debug.Log("Enter Laborator");
+        UIManager.Instance.questTitle.text = "새로운 스킬";
+        UIManager.Instance.questContent.text = "연구실을 지키는 사람을 모두 죽이세요!";
+
+        doQuest = true;
+
+        while (!isClear)
+        {
+            if (killedNpcCount >= 20)
+            {
+                isClear = true;
+                doQuest = false;
+            }
+            else
+            {
+                continue;
+            }
+        }
+
     }
 
     void ServerRoomQuest()
     {
         Debug.Log("Interfere Communication");
+        UIManager.Instance.questTitle.text = "외부로의 교신 방해";
+        UIManager.Instance.questContent.text = "사람이 외부로 교신을 전하고 있습니다. 방해하세요!";
     }
 
 }
