bceacdd4aaefcbd93296565051a038c1cfeb2eb5
Minkyu Kwak <lanilani7661@gmail.com>
2022-12-14 20:02:37 +0900

    PausePanel 재추가

--- Assets/01.Script/Manager/UIManager.cs
+++ Assets/01.Script/Manager/UIManager.cs
@@ -7,19 +7,65 @@ using TMPro;
 public class UIManager : MonoSingleton<UIManager>
 {
     [Header("QuestUI")]
-    public GameObject QuestPanel;
     public TextMeshProUGUI questTitle;
     public TextMeshProUGUI questContent;
+    [SerializeField]
+    private GameObject questPanel;
+
+
+    [Header("PauseUI")]
+    [SerializeField]
+    private GameObject pausePanel;
+    public bool isPause { private set; get; } = false;
 
     private void Start()
     {
-        ToggleQuestUI(false);
+        DisableAllPanels();
+    }
+
+    private void Update()
+    {
+        InputKey();
+    }
+
+    private void InputKey()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            TogglePausePanel();
+        }
     }
 
     public void ToggleQuestUI(bool toggle)
     {
-        QuestPanel.SetActive(toggle);
+        questPanel.SetActive(toggle);
     }
 
+    public void DisableAllPanels()
+    {
+        questPanel.SetActive(false);
+        pausePanel.SetActive(false);
+    }
 
+    public void TogglePausePanel()
+    {
+        pausePanel.SetActive(!pausePanel.activeSelf);
+        isPause = pausePanel.activeSelf;
+        MouseManager.Lock(!pausePanel.activeSelf);
+        MouseManager.Visible(pausePanel.activeSelf);
+        if (pausePanel.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void GameQuit()
+    {
+        Debug.Log("게임 종료");
+        Application.Quit();
+    }
 }
