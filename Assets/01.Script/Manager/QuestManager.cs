fc7f3551f1770510ad0828ea868062f4f172fa22
Minkyu Kwak <lanilani7661@gmail.com>
2022-12-17 18:43:40 +0900

    LaboratorQuest 완성

--- Assets/01.Script/Manager/QuestManager.cs
+++ Assets/01.Script/Manager/QuestManager.cs
@@ -35,8 +35,8 @@ public class QuestManager : MonoSingleton<QuestManager>
     private void EnterKitchen()
     {
         Debug.Log("Enter Kitchen");
-        UIManager.Instance.questTitle.text = "식당에서 여유롭게 아직 음식을 먹고 있어?";
-        UIManager.Instance.questContent.text = "식당에있는 사람을 모두 죽이세요!";
+        UIManager.Instance.questTitle.text = "Tasty People";
+        UIManager.Instance.questContent.text = "Defeat all people.";
 
         doQuest = true;
         UIManager.Instance.ToggleQuestUI(doQuest);
@@ -50,15 +50,22 @@ public class QuestManager : MonoSingleton<QuestManager>
         summonNurseCount = 8;
 
         InstantiateResearcher(summonResearcherCount, kitchenMobSpawnPos);
-        //yield return new WaitForSeconds(0.1f);
         InstantiateNurse(summonNurseCount, kitchenMobSpawnPos);
 
         while (!isClear)
         {
-            if (killedNpcCount >= 20)
+            for(int i = 0; i < kitchenMobSpawnPos.Length; ++i)
             {
-                isClear = true;
-                doQuest = false;
+                if(kitchenMobSpawnPos[i].childCount != 0)
+                {
+                    break;
+                }
+
+                if(i == kitchenMobSpawnPos.Length - 1)
+                {
+                    isClear = true;
+                    doQuest = false;
+                }
             }
 
             yield return new WaitForEndOfFrame();
@@ -72,8 +79,8 @@ public class QuestManager : MonoSingleton<QuestManager>
     private void EnterLaborator()
     {
         Debug.Log("Enter Laborator");
-        UIManager.Instance.questTitle.text = "새로운 스킬";
-        UIManager.Instance.questContent.text = "연구실을 지키는 사람을 모두 죽이세요!";
+        UIManager.Instance.questTitle.text = "New Skill..?";
+        UIManager.Instance.questContent.text = "Defeat all people \n who guard a laborator.";
 
         doQuest = true;
 
@@ -88,38 +95,42 @@ public class QuestManager : MonoSingleton<QuestManager>
         InstantiateResearcher(summonResearcherCount, LaboratorMobSpawnPos);
         InstantiateSoldier(summonSoldierCount, LaboratorMobSpawnPos);
 
-        while (!isClear)
+        for (int i = 0; i < LaboratorMobSpawnPos.Length; ++i)
         {
-            if (killedNpcCount >= 20)
+            if (LaboratorMobSpawnPos[i].childCount != 0)
+            {
+                break;
+            }
+
+            if (i == LaboratorMobSpawnPos.Length - 1)
             {
                 isClear = true;
                 doQuest = false;
             }
-
-            yield return new WaitForEndOfFrame();
         }
-
+        
         SaveManager.Instance.SaveToJson();
+        UIManager.Instance.ToggleQuestUI(doQuest);
         yield break;
     }
 
     void ServerRoomQuest()
     {
         Debug.Log("Interfere Communication");
-        UIManager.Instance.questTitle.text = "외부로의 교신 방해";
-        UIManager.Instance.questContent.text = "사람이 외부로 교신을 전하고 있습니다. 방해하세요!";
+        UIManager.Instance.questTitle.text = "Interfere with communication";
+        UIManager.Instance.questContent.text = "A person is communicating to the outside. \nGet in the way!";
     }
 
     void InstantiateResearcher(int researcherCount, Transform[] spawnPos)
     {
         int pivot = 0;
 
-        while (spawnPos[pivot].childCount != 0)
+        while (spawnPos[pivot].childCount != 0 && pivot < spawnPos.Length)
         {
             pivot++;
         }
 
-        for (int i = pivot; i < researcherCount; ++i)
+        for (int i = pivot; i < pivot + researcherCount; ++i)
         {
             Instantiate(researcherPrefab, spawnPos[i]);
         }
@@ -129,12 +140,12 @@ public class QuestManager : MonoSingleton<QuestManager>
     {
         int pivot = 0;
 
-        while(spawnPos[pivot].childCount != 0)
+        while(spawnPos[pivot].childCount != 0 && pivot < spawnPos.Length)
         {
             pivot++;
         }
 
-        for (int i = pivot; i < soldierCount; ++i)
+        for (int i = pivot; i < pivot + soldierCount; ++i)
         {
             Instantiate(soldierPrefab, spawnPos[i]);
         }
@@ -146,7 +157,7 @@ public class QuestManager : MonoSingleton<QuestManager>
 
         Debug.Log("InstantiateNurse");
 
-        while (spawnPos[pivot].childCount != 0)
+        while (spawnPos[pivot].childCount != 0 && pivot < spawnPos.Length)
         {
             Debug.Log(pivot);
             pivot++;
