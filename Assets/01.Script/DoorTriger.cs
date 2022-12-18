c0cb0bf6e48d556299844d8bebb771527eea1dfb
sehyeon1104 <sehyeon1104@naver.com>
2022-12-18 21:10:45 +0900

    포스트 프로세싱 추가

---
+++ Assets/01.Script/DoorTriger.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTriger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
