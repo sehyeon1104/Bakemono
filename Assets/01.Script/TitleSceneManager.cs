da3a6ffddcca016999e498a3b593ad74a2ddcdd4
Minkyu Kwak <lanilani7661@gmail.com>
2022-12-19 12:56:42 +0900

    타이틀씬 생성

---
+++ Assets/01.Script/TitleSceneManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject quitButton = null;
+    [SerializeField]
+    private GameObject quitPanel = null;
+    [SerializeField]
+    private GameObject helpButton = null;
+
+    private bool isQuitPanelActive = false;
+
+    private void Start()
+    {
+        isQuitPanelActive = false;
+    }
+
+    private void Update()
+    {
+        InputKey();
+    }
+
+    private void InputKey()
+    {
+        if (Input.anyKeyDown && !isQuitPanelActive)
+        {
+            GameStart();
+        }
+    }
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void DisableAllPanel()
+    {
+        quitPanel.SetActive(false);
+    }
+
+    public void ToggleQuitPanel()
+    {
+        quitPanel.SetActive(!quitPanel.activeSelf);
+        isQuitPanelActive = quitPanel.activeSelf;
+    }
+
+    public void GameQuit()
+    {
+        Application.Quit();
+    }
+}
