0a40300ffc1b5eb080db482006dd352fd2b82b71
Minkyu Kwak <lanilani7661@gmail.com>
2022-12-21 00:12:42 +0900

    패스워드 저장 및 패스워드 룸 저장

--- Assets/01.Script/Interface/Manager/SaveManager.cs
+++ Assets/01.Script/Interface/Manager/SaveManager.cs
@@ -21,7 +21,7 @@ public class SaveManager : MonoSingleton<SaveManager>
         if (!Directory.Exists(SAVE_PATH))
         {
             Directory.CreateDirectory(SAVE_PATH);
-            //File.WriteAllText(SAVE_PATH + SAVE_FILENAME, JsonUtility.ToJson(new User()), System.Text.Encoding.UTF8);
+            File.WriteAllText(SAVE_PATH + SAVE_FILENAME, JsonUtility.ToJson(new User()), System.Text.Encoding.UTF8);
         }
         LoadFromJson();
     }
@@ -32,26 +32,33 @@ public class SaveManager : MonoSingleton<SaveManager>
         //InvokeRepeating("SaveToJson", 1f, 60f);
     }
 
+    [ContextMenu("LoadFromJson")]
     private void LoadFromJson()
     {
         if (File.Exists(SAVE_PATH + SAVE_FILENAME))
         {
             string json = File.ReadAllText(SAVE_PATH + SAVE_FILENAME);
-            json = Crypto.AESDecrypt128(json);
-            user = JsonUtility.FromJson<User>(json);
-            LoadSpawnPos();
+            //json = Crypto.AESDecrypt128(json);
+            user = JsonUtility.FromJson<User>(json);    
+            Debug.Log("로딩완료");
+
+
             //SceneSerialization.ImportScene(SAVE_SCENE);
         }
     }
 
+    [ContextMenu("SaveToJson")]
     public void SaveToJson()
     {
         SavePlayerStat();
-        SaveSpawnPos();
+        //SaveSpawnPos();
 
         string json = JsonUtility.ToJson(user, true);
-        json = Crypto.AESEncrypt128(json);
+        //json = Crypto.AESEncrypt128(json);
         File.WriteAllText(SAVE_PATH + SAVE_FILENAME, json, System.Text.Encoding.UTF8);
+        Debug.Log("저장완료");
+
+
         //SAVE_SCENE = SceneSerialization.SerializeScene(SceneManager.GetActiveScene());
     }
 
@@ -62,19 +69,19 @@ public class SaveManager : MonoSingleton<SaveManager>
         CurrentUser.level = Monster.Instance.CurrentLevel;
     }
 
-    public void SaveSpawnPos()
-    {
-        CurrentUser.researcherMaleSpawnPos = GameManager.Instance.researcherMaleSpawnPos;
-        CurrentUser.researcherFemaleSpawnPos = GameManager.Instance.researcherFemaleSpawnPos;
-        CurrentUser.soldierSpawnPos = GameManager.Instance.soldierSpawnPos;
-    }
+    //public void SaveSpawnPos()
+    //{
+    //    CurrentUser.researcherMaleSpawnPos = GameManager.Instance.researcherMaleSpawnPos;
+    //    CurrentUser.researcherFemaleSpawnPos = GameManager.Instance.researcherFemaleSpawnPos;
+    //    CurrentUser.soldierSpawnPos = GameManager.Instance.soldierSpawnPos;
+    //}
 
-    public void LoadSpawnPos()
-    {
-        GameManager.Instance.researcherMaleSpawnPos = CurrentUser.researcherMaleSpawnPos;
-        GameManager.Instance.researcherFemaleSpawnPos = CurrentUser.researcherFemaleSpawnPos;
-        GameManager.Instance.soldierSpawnPos = CurrentUser.soldierSpawnPos;
-    }
+    //public void LoadSpawnPos()
+    //{
+    //    GameManager.Instance.researcherMaleSpawnPos = CurrentUser.researcherMaleSpawnPos;
+    //    GameManager.Instance.researcherFemaleSpawnPos = CurrentUser.researcherFemaleSpawnPos;
+    //    GameManager.Instance.soldierSpawnPos = CurrentUser.soldierSpawnPos;
+    //}
 
     //private void OnApplicationQuit()
     //{
